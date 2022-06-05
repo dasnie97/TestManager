638814f3e99db5dec0bf6ff750a51650665db375
dasnie97 <damiansnieda97@gmail.com>
2022-06-05 10:53:19 +0200

    Initial commit

---
+++ TestManager/MalfunctionReport.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace TestManager
+{
+    partial class MalfunctionReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.technicianLoginTextbox = new System.Windows.Forms.TextBox();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.actionTextbox = new System.Windows.Forms.TextBox();
+            this.descriptionTextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // technicianLoginTextbox
+            // 
+            this.technicianLoginTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.technicianLoginTextbox.Location = new System.Drawing.Point(342, 267);
+            this.technicianLoginTextbox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.technicianLoginTextbox.Name = "technicianLoginTextbox";
+            this.technicianLoginTextbox.Size = new System.Drawing.Size(116, 22);
+            this.technicianLoginTextbox.TabIndex = 16;
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.confirmButton.Location = new System.Drawing.Point(332, 308);
+            this.confirmButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(135, 38);
+            this.confirmButton.TabIndex = 15;
+            this.confirmButton.Text = "Potwierdź";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(369, 245);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Technik";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(334, 175);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Podjęte działania";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(358, 104);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Opis awarii";
+            // 
+            // actionTextbox
+            // 
+            this.actionTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.actionTextbox.Location = new System.Drawing.Point(283, 197);
+            this.actionTextbox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.actionTextbox.Multiline = true;
+            this.actionTextbox.Name = "actionTextbox";
+            this.actionTextbox.Size = new System.Drawing.Size(235, 44);
+            this.actionTextbox.TabIndex = 10;
+            // 
+            // descriptionTextbox
+            // 
+            this.descriptionTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.descriptionTextbox.Location = new System.Drawing.Point(283, 126);
+            this.descriptionTextbox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.descriptionTextbox.Multiline = true;
+            this.descriptionTextbox.Name = "descriptionTextbox";
+            this.descriptionTextbox.Size = new System.Drawing.Size(235, 44);
+            this.descriptionTextbox.TabIndex = 11;
+            // 
+            // MalfunctionReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.technicianLoginTextbox);
+            this.Controls.Add(this.confirmButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.actionTextbox);
+            this.Controls.Add(this.descriptionTextbox);
+            this.Name = "MalfunctionReport";
+            this.Text = "MalfunctionReport";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox technicianLoginTextbox;
+        private Button confirmButton;
+        private Label label5;
+        private Label label3;
+        private Label label2;
+        private TextBox actionTextbox;
+        private TextBox descriptionTextbox;
+    }
+}
