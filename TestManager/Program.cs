9297de0c962b2b454054a72e2feb48dde0387fe4
dasnie97 <damiansnieda97@gmail.com>
2022-12-12 20:05:25 +0100

    All configuration parameters are now stored in App.config which can be modified by user in GUI. MES, FTP and HTTP communication is loaded from separate code.

--- TestManager/Program.cs
+++ TestManager/Program.cs
@@ -1,4 +1,5 @@
 using System.Text.RegularExpressions;
+using System.Configuration;
 
 namespace TestManager
 {
@@ -14,52 +15,10 @@ namespace TestManager
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
 
-            var operatorName = LoadDefaultOperator();
-
-            if (operatorName == String.Empty)
+            if (!Convert.ToBoolean(ConfigurationManager.AppSettings.Get("AutoLogIn")))
                 Application.Run(new LogIn());
             else
-                Application.Run(new MainForm(operatorName, new Form()));
-        }
-
-        /// <summary>
-        /// Load default operator name from txt config file located in same folder as exe.
-        /// </summary>
-        static string LoadDefaultOperator()
-        {
-            var configPath = "config.txt";
-            var value = "";
-
-            if (!File.Exists(configPath))
-            {
-                MessageBox.Show($"Path {configPath} does not exist!");
-                Environment.Exit(0);
-            }
-
-            foreach (var line in File.ReadLines(configPath))
-            {
-                if (line.StartsWith("Operator"))
-                {
-                    value = line.Split("\t")[1].Trim().ToLower();
-                    break;
-                }
-                return "";
-            }
-
-            // Check if entered text has correct number of characters
-            if (value.Length != 6)
-            {
-                MessageBox.Show("Login operatora powinien sk³adaæ siê z 6 znaków!");
-                Environment.Exit(0);
-            }
-
-            // Check if entered text contains only allowed characters (Latin a-z)
-            if (Regex.Matches(value, @"[a-z]").Count != 6)
-            {
-                MessageBox.Show("Login powinien zawieraæ tylko litery bez polskich znaków!");
-                Environment.Exit(0);
-            }
-            return value;
+                Application.Run(new MainForm("dasnie", new Form()));
         }
     }
 }
