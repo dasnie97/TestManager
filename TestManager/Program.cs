ee270a1e2790588df6a6fa082a26d66f0ddd8ee7
dasnie97 <damiansnieda97@gmail.com>
2023-01-22 20:29:34 +0100

    Added check if app config file exists

--- TestManager/Program.cs
+++ TestManager/Program.cs
@@ -14,11 +14,25 @@ namespace TestManager
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-
-            if (!Convert.ToBoolean(ConfigurationManager.AppSettings.Get("AutoLogIn")))
-                Application.Run(new LogIn());
+            if (ConfigFileIsPresent())
+            {
+                if (!Convert.ToBoolean(ConfigurationManager.AppSettings.Get("AutoLogIn")))
+                    //Application.Run(new LogIn());
+                    Application.Run(new MainForm("operat", new Form()));
+                else
+                    Application.Run(new MainForm("operat", new Form()));
+            }
             else
-                Application.Run(new MainForm("operat", new Form()));
+            {
+                MessageBox.Show("Cannot find config file!");
+                Environment.Exit(0);
+            }
+            
+        }
+        public static bool ConfigFileIsPresent()
+        {
+            var pathToAppConfig = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, "TestManager.dll.config");
+            return File.Exists(pathToAppConfig);
         }
     }
 }
