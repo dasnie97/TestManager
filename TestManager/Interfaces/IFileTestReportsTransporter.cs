4842f6ae898e36359729f4c3ec2224027d4dd2f2
dasnie97 <damiansnieda97@gmail.com>
2023-01-22 20:11:07 +0100

    Initial commit

---
+++ TestManager/Interfaces/IFileTestReportsTransporter.cs
@@ -0,0 +1,14 @@
+﻿using ProductTest.Common;
+using ProductTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestManager.Interfaces;
+
+public interface IFileTestReportsTransporter
+{
+    public void TransportTestReports();
+}
---
+++ TestManager/Transporters/NoFilesTransporter.cs
@@ -0,0 +1,23 @@
+﻿using ProductTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TestManager.Helpers;
+using TestManager.Interfaces;
+
+namespace TestManager.Transporters;
+
+public class NoFilesTransporter : IFileTestReportsTransporter
+{
+    public NoFilesTransporter()
+    {
+        
+    }
+    public void TransportTestReports()
+    {
+        // No files are transported
+    }
+}
