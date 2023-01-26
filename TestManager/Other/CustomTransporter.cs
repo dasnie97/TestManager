73b81ca08411c09381182e289c7337f02ea24546
dasnie97 <damiansnieda97@gmail.com>
2023-01-26 18:27:16 +0100

    Modified CustomTransporter to change file test report content with regular expression.

--- TestManager/Other/CustomTransporter.cs
+++ TestManager/Other/CustomTransporter.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TestManager.Helpers;
 using TestManager.Interfaces;
@@ -17,37 +18,20 @@ public class CustomTransporter : TransporterBase
     {
         IFileLoader fileLoader = new FileLoader();
         IEnumerable<FileTestReport> loaded = fileLoader.GetTestReportFiles(Config.Instance.InputDir);
-        List<FileTestReport> modified = new List<FileTestReport>();
 
         foreach (var testReport in loaded)
         {
-            var testStepsListed = testReport.TestSteps.ToList();
-
-            foreach (var testStep in testStepsListed)
-            {
-                if (testStep.Name == "3.1 Setup_iot:psk:remote:encryptedsecret")
-                {
-                    var index = testStepsListed.IndexOf(testStep);
-                    var newTestStep = TestStep.Create("iot:psk:remote:encryptedsecret",
-                                                        testStep.DateTimeFinish,
-                                                        testStep.Status,
-                                                        testStep.Type,
-                                                        testStep.Value,
-                                                        testStep.Unit,
-                                                        testStep.LowerLimit,
-                                                        testStep.UpperLimit,
-                                                        testStep.Failure);
-
-                    testStepsListed[index] = newTestStep;
-                    break;
-                }
-            }
-            var newTestReport = FileTestReport.Create(testReport.SerialNumber, testReport.Workstation.Name, testStepsListed, testReport.FilePath);
-            //TODO: creating new files may change file name leading to exceptions. Solve this by reading file name and content, replace content and save
-            // with same name.
-            newTestReport.SaveReport(Config.Instance.InputDir);
-            modified.Add(newTestReport);
+            ReplaceFileContent(testReport.FilePath);
         }
-        return modified;
+
+        return fileLoader.GetTestReportFiles(Config.Instance.InputDir);
+    }
+
+    private void ReplaceFileContent(string filePath)
+    {
+        Regex regex = new Regex("TestName.*?iot:psk:remote:encryptedsecret");
+        var originalFileContent = File.ReadAllText(filePath);
+        var newFileContent = regex.Replace(originalFileContent, "TestName:\tiot:psk:remote:encryptedsecret");
+        File.WriteAllText(filePath, newFileContent);
     }
 }
