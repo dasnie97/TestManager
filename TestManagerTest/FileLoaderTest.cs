a306450c5972afc481338d6f8ac06a0ca2becb04
dasnie97 <damiansnieda97@gmail.com>
2023-01-28 18:31:31 +0100

    Added test for empty file loading

--- TestManagerTest/FileLoaderTest.cs
+++ TestManagerTest/FileLoaderTest.cs
@@ -1,34 +1,48 @@
+using ProductTest.Common;
 using ProductTest.Models;
 using ProductTestTest;
+using TestManager.FileHelpers;
 using TestManager.Helpers;
-using TestManager.Interfaces;
 
 namespace TestManagerTest;
 
 public class FileLoaderTest : IDisposable
 {
+    private string tempDir;
     private string okFilePath;
-    private string errorFilePath;
+    private string emptyFilePath;
     public FileLoaderTest()
     {
+        tempDir = Path.Combine(Directory.GetCurrentDirectory(), "tempDir");
+        Directory.CreateDirectory(tempDir);
+
         var okFile = RandomTestReport.ArrangeTestReportWithDefaultAndRandomData();
-        okFile.SaveReport(Directory.GetCurrentDirectory());
+        okFile.SaveReport(tempDir);
         okFilePath = okFile.FilePath;
-        var errorFile = RandomTestReport.ArrangeTestReportWithDefaultAndRandomData();
-        errorFile.SaveReport(Directory.GetCurrentDirectory());
-        errorFilePath = errorFile.FilePath;
+
+        var emptyFile = FileTestReport.Create("123123123",
+                                            "TestW",
+                                            new List<TestStepBase>()
+                                            {
+                                                TestStep.Create("Test1", DateTime.Now, TestStatus.Passed)
+                                            });
+        emptyFilePath = Path.Combine(tempDir, emptyFile.FileName);
+        File.Create(emptyFilePath).Close();
     }
 
     public void Dispose()
     {
-        if (File.Exists(okFilePath)) { File.Delete(okFilePath); }
-        if (File.Exists(errorFilePath)) { File.Delete(errorFilePath); } 
+        Directory.Delete(tempDir, true);
     }
 
     [Fact]
     public void TestIfCorrectFilesAreLoaded()
     {
         IFileLoader loader = new FileLoader();
-        var loadedFiles = loader.GetFiles(Directory.GetCurrentDirectory());
+
+        var loadedFiles = loader.GetFiles(tempDir);
+
+        Assert.True(loadedFiles.Count() == 1);
+        Assert.Equal(loadedFiles.First(), okFilePath);
     }
 }
