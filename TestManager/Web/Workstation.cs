852c47b156f91690b5427d508e0d2265157e8598
dasnie97 <damiansnieda97@gmail.com>
2023-03-11 09:55:51 +0100

    Workstation model

---
+++ TestManager/Web/Workstation.cs
@@ -0,0 +1,23 @@
+﻿using ProductTest.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestManager.Configuration;
+
+namespace TestManager.Web;
+
+public class Workstation : IWorkstation
+{
+    private readonly IWorkstationConfig _workstationConfig;
+
+    public string Name { get; set ; }
+    public string OperatorName { get; set ; }
+
+    public Workstation(IWorkstationConfig workstationConfig)
+    {
+        _workstationConfig = workstationConfig;
+        Name = _workstationConfig.TestStationName;
+    }
+}
