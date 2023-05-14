dc597e0380a5c82b3c321f495299f9d6b4496c72
dasnie97 <damiansnieda97@gmail.com>
2023-05-14 17:08:47 +0200

    Removed 2 unused properties

--- TestManager/Configuration/IDirectoryConfig.cs
+++ TestManager/Configuration/IDirectoryConfig.cs
@@ -5,7 +5,5 @@ public interface IDirectoryConfig
     public string InputDir { get; set; }
     public string OutputDir { get; set; }
     public string CopyDir { get; set; }
-    public bool IsCopyingEnabled { get; set; }
-    public string DateNamedCopyDirectory { get; set; }
     public void ReadConfig();
 }
