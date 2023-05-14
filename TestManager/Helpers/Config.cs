123e7a449242547cb2d87013748262ebcdf69552
dasnie97 <damiansnieda97@gmail.com>
2023-05-14 17:10:30 +0200

    Removed 2 unused properties, moved creating date named copy directory to responsible service

--- TestManager/Helpers/Config.cs
+++ TestManager/Helpers/Config.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
+using TestManager.Configuration;
 
-namespace TestManager.Configuration;
+namespace TestManager.Helpers;
 
 public class Config : IWorkstationConfig, IWebConfig, IDirectoryConfig
 {
@@ -14,9 +15,6 @@ public class Config : IWorkstationConfig, IWebConfig, IDirectoryConfig
     public string InputDir { get; set; }
     public string OutputDir { get; set; }
     public string CopyDir { get; set; }
-    public string DateNamedCopyDirectory { get; set; }
-    public bool IsCopyingEnabled { get; set; } = false;
-
 
     private readonly IConfiguration _configuration;
 
@@ -24,28 +22,17 @@ public class Config : IWorkstationConfig, IWebConfig, IDirectoryConfig
     {
         _configuration = configuration;
         ReadConfig();
-        CreateDateNamedCopyDir();
     }
 
     public Config()
     {
-        
+
     }
 
     public void ReadConfig()
     {
-        IConfigurationSection section = _configuration.GetSection(this.GetType().Name);
-        section.Bind(this);
-    }
-
-    private void CreateDateNamedCopyDir()
-    {
-        if (Directory.Exists(CopyDir))
-        {
-            IsCopyingEnabled = true;
-            var dateNamedDirectory = Path.Combine(CopyDir, $"{DateTime.Now.Year}_{DateTime.Now.Month.ToString("00")}");
-            Directory.CreateDirectory(dateNamedDirectory);
-            DateNamedCopyDirectory = dateNamedDirectory;
-        }
+        _configuration.Bind("WorkstationConfig", this);
+        _configuration.Bind("WebConfig", this);
+        _configuration.Bind("DirectoryConfig", this);
     }
 }
