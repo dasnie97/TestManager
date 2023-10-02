42b9c207f4a18d1cef658afc2f28e5c621515c56
dasnie97 <damiansnieda97@gmail.com>
2023-10-02 08:40:22 +0200

    Added exception handler for HTTP POST in AllFilesAdapter. Now application wont shut down on HttpRequestException and SocketException.

--- TestManager/Forms/LogInForm.Designer.cs
+++ TestManager/Forms/LogInForm.Designer.cs
@@ -28,91 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogInForm));
-            this.label1 = new System.Windows.Forms.Label();
-            this.operatorLoginTextbox = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.acceptButton = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
+            label1 = new Label();
+            operatorLoginTextbox = new TextBox();
+            acceptButton = new Button();
+            SuspendLayout();
             // 
             // label1
             // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(50, 90);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(214, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Logowanie operatora";
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 18);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(287, 31);
+            label1.TabIndex = 0;
+            label1.Text = "Logowanie operatora";
             // 
             // operatorLoginTextbox
             // 
-            this.operatorLoginTextbox.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.operatorLoginTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.operatorLoginTextbox.Location = new System.Drawing.Point(90, 128);
-            this.operatorLoginTextbox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.operatorLoginTextbox.Name = "operatorLoginTextbox";
-            this.operatorLoginTextbox.Size = new System.Drawing.Size(135, 26);
-            this.operatorLoginTextbox.TabIndex = 1;
-            this.operatorLoginTextbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.operatorLoginTextbox_KeyPress);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(101, 12);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(113, 75);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
+            operatorLoginTextbox.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            operatorLoginTextbox.Location = new Point(88, 64);
+            operatorLoginTextbox.Margin = new Padding(4, 3, 4, 3);
+            operatorLoginTextbox.Name = "operatorLoginTextbox";
+            operatorLoginTextbox.Size = new Size(135, 31);
+            operatorLoginTextbox.TabIndex = 1;
+            operatorLoginTextbox.KeyPress += operatorLoginTextbox_KeyPress;
             // 
             // acceptButton
             // 
-            this.acceptButton.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.acceptButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.acceptButton.Location = new System.Drawing.Point(84, 195);
-            this.acceptButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.acceptButton.Name = "acceptButton";
-            this.acceptButton.Size = new System.Drawing.Size(147, 37);
-            this.acceptButton.TabIndex = 4;
-            this.acceptButton.Text = "Zatwierdź";
-            this.acceptButton.UseVisualStyleBackColor = true;
-            this.acceptButton.Click += new System.EventHandler(this.acceptButton_Click);
+            acceptButton.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            acceptButton.Location = new Point(82, 111);
+            acceptButton.Margin = new Padding(4, 3, 4, 3);
+            acceptButton.Name = "acceptButton";
+            acceptButton.Size = new Size(147, 37);
+            acceptButton.TabIndex = 4;
+            acceptButton.Text = "Zatwierdź";
+            acceptButton.UseVisualStyleBackColor = true;
+            acceptButton.Click += acceptButton_Click;
             // 
-            // LogIn
+            // LogInForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(312, 246);
-            this.ControlBox = false;
-            this.Controls.Add(this.acceptButton);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.operatorLoginTextbox);
-            this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.Name = "LogIn";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Logowanie";
-            this.TopMost = true;
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.LoginForm_KeyDown);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(312, 174);
+            ControlBox = false;
+            Controls.Add(acceptButton);
+            Controls.Add(operatorLoginTextbox);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            KeyPreview = true;
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "LogInForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Logowanie";
+            TopMost = true;
+            KeyDown += LoginForm_KeyDown;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox operatorLoginTextbox;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button acceptButton;
+        private Label label1;
+        private TextBox operatorLoginTextbox;
+        private Button acceptButton;
     }
 }
